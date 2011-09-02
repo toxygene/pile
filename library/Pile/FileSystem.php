ae1022dbeba655780a51c591534853aa17a43b46
Justin Hendrickson <justin.hendrickson@gmail.com>
2011-09-01 20:28:20 -0500

    Added new methods and updated license to BSD-3 clause

--- library/Pile/FileSystem.php
+++ library/Pile/FileSystem.php
@@ -2,25 +2,31 @@
 /**
  * Pile
  *
- * Copyright (c) 2011 Justin Hendrickson
+ * Copyright (c) 2011, Justin Hendrickson
+ * All rights reserved.
  *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *     * Redistributions of source code must retain the above copyright notice,
+ *       this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *     * The names of its contributors may not be used to endorse or promote
+ *       products derived from this software without specific prior written
+ *       permission.
  *
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
- * SOFTWARE.
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
  */
 
 namespace Pile;
@@ -175,6 +181,37 @@ class FileSystem
     {
         return $this->_run(function() use ($path) {
             return filetype($path);
+        });
+    }
+
+    /**
+     * Get the contents of a file
+     *
+     * @param string $path
+     * @param boolean $useIncludePath
+     * @param resource $context
+     * @param integer $offset
+     * @param integer maxlen
+     * @return string
+     * @throws Exception
+     */
+    public function getContents($path, $useIncludePath = false, $context = null, $offset = -1, $maxlen = null)
+    {
+        return $this->_run(function() use ($path, $useIncludePath, $context, $offset, $maxlen) {
+            return file_get_contents($path, $useIncludePath, $context, $offset, $maxlen);
+        });
+    }
+
+    /**
+     * Get the current working directory
+     *
+     * @return string
+     * @throws Exception
+     */
+    public function getcwd()
+    {
+        return $this->_run(function() {
+            return getcwd();
         });
     }
 
@@ -334,6 +371,25 @@ class FileSystem
             } else {
                 return rename($from, $to);
             }
+        });
+
+        return $this;
+    }
+
+    /**
+     * Write a string to a file
+     *
+     * @param string $path
+     * @param string $contents
+     * @param integer $flags
+     * @param resource context
+     * @return FileSystem
+     * @throws Exception
+     */
+    public function putContents($path, $contents, $flags = 0, $context = null)
+    {
+        $this->_run(function() use ($path, $contents, $flags, $context) {
+            return file_put_contents($path, $contents, $flags, $context);
         });
 
         return $this;
